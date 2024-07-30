c5db606ade49cd55a57515a2b0229526c520ae65
David Huang <djhuang636@gmail.com>
2024-07-30 18:56:31 -0400

    complete exercises 02-04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const numToRemove of args) {
+        while (arr.indexOf(numToRemove) !== -1) {
+            arrLeft = arr.slice(0, arr.indexOf(numToRemove));
+            arrRight = arr.slice(arr.indexOf(numToRemove)+1);
+            arr = arrLeft.concat(arrRight);
+        }
+    }      
+    return arr;
 };
 
 // Do not edit below this line
