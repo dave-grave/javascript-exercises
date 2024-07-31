196f7c1867c0cf7b8a81ba605df19f0798af208a
David Huang <djhuang636@gmail.com>
2024-07-31 09:11:54 -0400

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, secondInt) {
+    if (firstInt < 0 || 
+        secondInt < 0 || 
+        Number(firstInt) !== firstInt || 
+        Number(secondInt) !== secondInt ||
+        firstInt % 1 !== 0 ||
+        secondInt % 1 !== 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let largerInt = (firstInt > secondInt) ? firstInt : secondInt;
+    let smallerInt = (firstInt < secondInt) ? firstInt : secondInt;
+    for (let i = smallerInt; i <= largerInt; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
