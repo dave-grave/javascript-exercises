0d44d6a3b5d28fe25ed018dda2f05e58922122f5
David Huang <djhuang636@gmail.com>
2024-08-06 12:30:13 -0400

    complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((sum, item) => sum + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((factorial, item) => factorial * item, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	product = 1;
+  for (let i=0; i < a; i++) {
+    product *= i + 1;
+  }
+  return product;
 };
 
 // Do not edit below this line
