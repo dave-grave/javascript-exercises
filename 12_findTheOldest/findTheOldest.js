63d2d24b628ac5a9793f82fdd672006d93572e76
David Huang <djhuang636@gmail.com>
2024-08-06 13:30:17 -0400

    complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    for (let item of arr) {
+        if (item.yearOfDeath === undefined) {
+            item.yearOfDeath = 2024;
+        }
+    }
+    arr.sort((a, b) => (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth));
+    return arr[arr.length-1];
 };
 
 // Do not edit below this line
