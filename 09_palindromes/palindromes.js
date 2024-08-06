3f3ff9926a24fb96ea92ac25d3c8a66556489ec8
David Huang <djhuang636@gmail.com>
2024-08-06 12:44:41 -0400

    complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let prohibitedChars = [',', '.', '!', ' '];
+    let normalString = string.toLowerCase()
+                             .split('')
+                             .filter((item) => !prohibitedChars.includes(item))
+                             .join('');
 
+    let reversedString = string.toLowerCase()
+                               .split('')
+                               .filter((item) => !prohibitedChars.includes(item))
+                               .reverse()
+                               .join('');
+    return normalString === reversedString;
+                 
 };
 
 // Do not edit below this line
