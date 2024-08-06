a4649da976e2e1939eb1b3de62cb247cc17c0795
David Huang <djhuang636@gmail.com>
2024-08-06 13:20:51 -0400

    complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(nthNum) {
+    let f_0 = 0;
+    let f_1 = 1;
+    for (let i = 0; i < nthNum; i++) {
+        if (i % 2 === 0) {
+            f_0 += f_1;
+        } else {
+            f_1 += f_0; 
+        }
+    }
+    if (nthNum < 0) {
+        return "OOPS";}
+    else if (nthNum % 2 === 0) 
+        {return f_0;}
+    else 
+        {return f_1};
 };
 
 // Do not edit below this line
